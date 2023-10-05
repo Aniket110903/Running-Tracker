98160cd2b43caefe1818a2e18ede07ff3f00f186
Aniket Goyal <129101494+Aniket110903@users.noreply.github.com>
2023-10-06 00:18:19 +0530

    almost

--- Running Tracker/index.js
+++ Running Tracker/index.js
@@ -1,5 +1,11 @@
 
 let entries = [];
+let sum = 0;
+let avg = 0;
+let avgcount=0;
+let count =0;
+let i=0;
+let highest =0;
 const entriesWrapper = document.querySelector("#entries");
 
 function addNewEntry(newEntry){
@@ -7,8 +13,32 @@ function addNewEntry(newEntry){
     const listItems = document.createElement('li');
     const listValue = document.createTextNode(newEntry);
     listItems.appendChild(listValue);
-
     entriesWrapper.appendChild(listItems);
+}
+function updateNewEntry(newEntry){
+    sum = sum + newEntry;
+    count++;
+    if(count >7){
+        sum = sum - entries[i];
+        i++;
+    }
+    avgcount++;
+    if(avgcount>7 ){
+        avgcount = 7;
+    }
+    avg = sum/avgcount;
+    if(count>7){
+        highest = 0 ;
+    }
+    for(let j = i ; j< entries.length; j++){
+        if(entries[j]> highest){
+            highest = entries[j];
+        }
+    }
+
+    document.getElementById("total").innerHTML =  sum; 
+    document.getElementById("average").innerHTML = avg.toFixed(2);
+    document.getElementById("high").innerHTML = highest.toFixed(2);
 }
 
 function handleSubmit(event){
@@ -18,8 +48,7 @@ function handleSubmit(event){
     document.querySelector("form").reset();
     entries.push(entry);
     addNewEntry(entry);
+    updateNewEntry(entry);
 }
 
-const form = document
-.querySelector("form")
-.addEventListener("submit",handleSubmit);
+const form = document.querySelector("form").addEventListener("submit",handleSubmit);
